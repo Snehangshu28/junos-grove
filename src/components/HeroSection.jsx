d9c360046814b471a768df35441d68c08a877988
snehangshu <snehangshu1228@gmail.com>
2025-06-01 15:24:42 +0530

    image add

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './HeroSection.css';
 import jungleBg from '../assets/Background3.svg';
 import leftImage from '../assets/left.png';
-import rightImage from '../assets/Right.png';
+import rightImage from '../assets/right.png';
 import footerImage from '../assets/footer.png';
 
 export default function HeroSection() {
